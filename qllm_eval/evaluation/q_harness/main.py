0292a9a525039aefd42b8c601d75fea53e6b2a0e
akshat652256 <pkmnakshat@gmail.com>
2025-11-01 10:25:28 +0530

    New update

--- qllm_eval/evaluation/q_harness/main.py
+++ qllm_eval/evaluation/q_harness/main.py
@@ -2,10 +2,12 @@ import os
 import json
 import argparse
 
+
 from qllm_eval.quantization.quant_wrapper import quantize_model
 from qllm_eval.utils import build_model_and_enc
 from qllm_eval.evaluation.q_harness.lm_eval_adaptor import LMEvalAdaptor
 from lm_eval import evaluator
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", type=str, help="path of the hf model")
@@ -22,34 +24,41 @@ parser.add_argument("--kv_bit", type=int, default=16)
 args = parser.parse_args()
 
 
+
 def main():
     print("* Quantization Format: kv_{}_w_{}_a_{}".format(args.kv_bit, args.w_bit, args.a_bit))
     if 'falcon' in args.model_path.lower():
         args.kv_group_size = 64
         args.w_group_size = 64
 
+
     # a hack here to auto set model group
     model, enc = build_model_and_enc(args.model_path, args.use_flash_attn, args.kv_bit, args.kv_group_size)
 
+
     # quantize model
     model = quantize_model(model, args)
+
 
     # # save the quantized model
     # if args.output_path:
     #     model.save_pretrained(args.output_path, safe_serialization=False)
     #     enc.save_pretrained(args.output_path)
 
+
     # evaluation
     lm_eval_model = LMEvalAdaptor(args.model_path, model, enc, 1)
 
+
     if args.tasks is not None:
         task_names = args.tasks.split(",")
+
 
         results = evaluator.simple_evaluate(
             model=lm_eval_model,
             tasks=task_names,
             batch_size=1,
-            no_cache=True,
+            cache_requests=None,  # Changed from no_cache=True
             num_fewshot=0,
         )
         # print(results)
@@ -61,6 +70,7 @@ def main():
                 os.makedirs("{}/{}".format(task_name, args.model_path))
             with open(output_path, 'w') as f:
                 f.write(json.dumps(results['results'][task_name]) + "\n")
+
 
 
 if __name__ == "__main__":
